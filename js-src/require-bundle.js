cd8bcb8dda0ccf42ff6a7e015a6a85c076857262
Diego Versiani <diegoversiani@gmail.com>
2020-11-17 16:26:16 +0100

    Add `deregister` method

--- js-src/require-bundle.js
+++ js-src/require-bundle.js
@@ -110,6 +110,23 @@
 
 
 	/**
+	 * Deregister a bundle of resources
+	 *
+	 * @param   {String}  bundleId  Bundle ID
+	 */
+	_publicMethods.deregister = function( bundleId ) {
+		// Already registered
+		if ( _publicMethods.hasBundle( bundleId ) ) { 
+			delete _bundles[ bundleId ];
+			return true;
+		};
+
+		return false;
+	};
+
+
+
+	/**
 	 * Load bundle of dependencies using LoadJS
 	 *
 	 * @param   {Array}     bundleIds   Array of Bundle IDs to load
